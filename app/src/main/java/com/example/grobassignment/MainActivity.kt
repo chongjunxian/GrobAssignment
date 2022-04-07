58866c77dd30fa0d51918493b10a7d9f7971fda8
james yow jun wei <jamesyjw-wm19@student.tarc.edu.my>
2022-04-07 22:45:35 +0800

    testing 2

--- app/src/main/java/com/example/grobassignment/MainActivity.kt
+++ app/src/main/java/com/example/grobassignment/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         fun (){
             //hello FK jun xian
+            //JX love Peter
         }
     }
 }
